d3520f64a5100c37f4c7e25948ca3a94db8da395
xiaoran <510495266@qq.com>
2013-07-21 22:09:30 +0800

    It is my first testing

---
+++ UIDeviceAdditions.m
@@ -0,0 +1,14 @@
+//
+//  UIDeviceAdditions.m
+//  Test
+//
+//  Created by mac on 13-7-17.
+//  Copyright (c) 2013年 xiaoran. All rights reserved.
+//
+
+#import "UIDeviceAdditions.h"
+
+@implementation UIDevice(Additions)
+
+
+@end
